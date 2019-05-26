03fa6963778f9848d11d8fa7cd38e09aaafb1dc4
flat0531 <flat053117117@gmail.com>
2019-05-26 23:17:12 +0900

    バージョンあっぷ

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,16 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class AList
+{
+    public int b;
+}
+
 
 public class Test : MonoBehaviour
 {
+    private List<AList> _aList = new List<AList>();
     // Start is called before the first frame update
     void Start() {
+        for (int i = 0; i < 5; i++) {
+            AList a = new AList();
+            a.b = i;
+            _aList.Add(a);
+        }
 
+        Debug.Log("普通");
+        var f = _aList.Where(x => x.b > 3)
+            .Select(x => x.b + 5);
+        //_aList.ForEach(x => x.b = 0);
+        if (_aList.Any()) {
+
+        }
+        foreach (var a in f) {
+            Debug.Log(a);
+        }
     }
 
-    // Update is called once per frame
-    void Update() {
 
-    }
 }
